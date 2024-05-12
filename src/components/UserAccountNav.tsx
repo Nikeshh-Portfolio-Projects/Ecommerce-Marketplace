5514a6ab3300d5003cfadf0580df755e05658746
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2024-05-11 21:26:32 -0400

    Layout changes

---
+++ src/components/UserAccountNav.tsx
@@ -0,0 +1,10 @@
+import { User } from '@/payload-types';
+
+const UserAccountNav = ({ user }: { user: User }) => {
+    return (
+        <>
+        </>
+    );
+}
+
+export default UserAccountNav;
