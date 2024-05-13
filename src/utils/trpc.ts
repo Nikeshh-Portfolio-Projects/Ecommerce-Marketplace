0c65cd5f0e76476b3a11ade051aed8db2b3eb380
Nikeshh Vijayabaskaran <nikeshhbaskaran01@gmail.com>
2024-05-13 15:34:11 -0400

    Payload CMS integration

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,4 +1,4 @@
-import type { AppRouter } from "@/app/api/trpc/index";
+import type { AppRouter } from "@/app/(app)/api/trpc/index";
 import { createTRPCReact } from "@trpc/react-query";
 
 export const trpc = createTRPCReact<AppRouter>();
